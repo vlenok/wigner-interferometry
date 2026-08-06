99423b340754c7d6542e6ade81c3373084031174
vlenok <47772986+vlenok@users.noreply.github.com>
2026-08-06 21:40:23 +0200

    adding license to sigproc.py

--- sigproc.py
+++ sigproc.py
@@ -1,3 +1,19 @@
+# sigproc - a part of the wigner-interferometry project.
+# Copyright (C) 2026 Vladimir Lenok
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
